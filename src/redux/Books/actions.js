ad616b1d1d7bcba36b3b15b4a064199dc878468b
Lucas Nantes <nantes332@gmail.com>
2022-05-09 09:06:13 -0300

    cleaned up

--- src/redux/Books/actions.js
+++ src/redux/Books/actions.js
@@ -8,11 +8,3 @@ export const appBookListRequest = (page) => ({
   type: GET_BOOK_LIST,
   payload: page,
 });
-
-export const appNextBookList = () => ({
-  type: GET_NEXT_BOOK_LIST,
-});
-
-export const appPreviousBookList = () => ({
-  type: GET_PREVIOUS_BOOK_LIST,
-});
--- src/redux/Books/sagas.js
+++ src/redux/Books/sagas.js
@@ -1,9 +1,5 @@
 import { all, call, takeLatest, put } from "redux-saga/effects";
-import {
-  GET_BOOK_LIST,
-  GET_NEXT_BOOK_LIST,
-  GET_PREVIOUS_BOOK_LIST,
-} from "./constants";
+import { GET_BOOK_LIST } from "./constants";
 import { getBooks } from "../../services/http_services";
 import { actionBooks, actionLoading, actionReady } from "./slices";
 
@@ -23,22 +19,4 @@ function* handleGetBookList(action) {
   }
 }
 
-function* handleNextBookList() {
-  try {
-    const response = yield call(getBooks);
-    console.log(response);
-  } catch (e) {}
-}
-
-function* handlePreviousBookList() {
-  try {
-    const response = yield call(getBooks);
-    console.log(response);
-  } catch (e) {}
-}
-
-export default all([
-  takeLatest(GET_BOOK_LIST, handleGetBookList),
-  takeLatest(GET_NEXT_BOOK_LIST, handleNextBookList),
-  takeLatest(GET_PREVIOUS_BOOK_LIST, handlePreviousBookList),
-]);
+export default all([takeLatest(GET_BOOK_LIST, handleGetBookList)]);
